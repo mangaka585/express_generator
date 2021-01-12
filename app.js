ce1337fdf56881cb6a2193e6e64c21e62fbaabe4
Alexander <mangaka585@gmail.com>
2021-01-13 00:01:35 +0300

    Basic Authentication

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -34,6 +37,30 @@ app.use('/leaders',leaderRouter);
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
 
 connect.then((db) => {
     console.log("Connected correctly to server");
