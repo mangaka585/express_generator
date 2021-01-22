9ce588fa1188a4cfb7dd77c1d1a234cb1be28014
Alexander <mangaka585@gmail.com>
2021-01-23 00:36:25 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '865853780872697',
+        clientSecret: 'b628e48b99abfcadac71c805f62fb522'
+    }
 }
