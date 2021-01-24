97e0c5bc94a4c7126cd69623041099dba02b4aa4
Alexander <mangaka585@gmail.com>
2021-01-24 03:50:01 +0300

    Fnal Assignment 4

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
+const Favorites = require('./models/favourite');
 
 const app = express();
 
@@ -53,6 +55,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
